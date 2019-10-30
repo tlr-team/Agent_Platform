d1fe714ef9c6ccf7b1b1b6b9dc1903ebb34adc9d
Roberto Marti Cedeño <r.marti@estudiantes.matcom.uh.cu>
2019-10-30 19:05:23 -0400

    First approach to Port Forwarding

---
+++ Client/config.py
@@ -0,0 +1,8 @@
+Log_Path = "/var/log/agent_platform/logs/"
+Error_Path =  "/var/log/agent_platform/errors/"
+Config_Path = "/etc/agent_platform/client_config/"
+First_Run = False
+
+Service_Port = 8888
+Server_Ip = "192.168.2.16"
+Server_Port = 8000
---
+++ Client/consumer.py
@@ -0,0 +1,36 @@
+from config import Log_Path,Error_Path,Service_Port,Server_Port,Server_Ip
+from socket import socket, SOCK_DGRAM, SOL_SOCKET, SO_REUSEADDR,SOCK_STREAM
+from json import dumps, loads
+
+server = (Server_Ip,Server_Port)
+producers = []
+
+Resource = "A"
+
+local = socket(type = SOCK_STREAM)
+local.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
+local.bind(('localhost', Service_Port))
+local.listen(100)
+
+while True:
+    client, addr = local.accept()
+    cp = socket(type = SOCK_STREAM)
+    msg = client.recv(2048)
+    print(msg)
+    cp.connect(server)
+    cp.send(msg)
+    time = 60
+    while(time):
+        msgcp = cp.recv(2048)
+        client.send(msgcp)
+        print(msgcp)
+    cp.close()
+    client.close()
+
+
+# def Get_Data(s):
+#     msg, addr = s.recvfrom(2048)
+#     producers = loads(msg)
+
+
+# def Connect_To_Cp(Cpip,cpport):
