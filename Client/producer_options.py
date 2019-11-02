76934eeb77eedead87bf0afe4c6f7f7ec88d0327
Roberto Marti Cedeño <r.marti@estudiantes.matcom.uh.cu>
2019-11-02 19:42:04 -0400

    Added producer agent

---
+++ Client/producer_options.py
@@ -0,0 +1,8 @@
+Log_Path = "/var/log/agent_platform/logs/"
+Error_Path =  "/var/log/agent_platform/errors/"
+Config_Path = "/etc/agent_platform/client_config/"
+First_Run = False
+
+Service_Port = 8888
+Server_Ip = "190.92.127.83"
+Server_Port = 80
