7f2b981e5093b15c5389e26fbc03e11605edc2b3
Roberto Marti Cedeño <r.marti@estudiantes.matcom.uh.cu>
2019-11-02 19:41:30 -0400

    Added comments to consumer code

--- Client/consumer.py
+++ Client/consumer.py
@@ -2,28 +2,39 @@ from config import Log_Path,Error_Path,Service_Port,Server_Port,Server_Ip
 from socket import socket, SOCK_DGRAM, SOL_SOCKET, SO_REUSEADDR,SOCK_STREAM
 from json import dumps, loads
 
+# Dirección del servidor de la plataforma de agentes
 server = (Server_Ip,Server_Port)
+# Lista de productores pedida
 producers = []
-
+# Recurso buscado
 Resource = "A"
 
+# socket de servicio (local) "localhost:8000 para simplificar el acceso del cliente"
 local = socket(type = SOCK_STREAM)
 local.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
 local.bind(('localhost', Service_Port))
-local.listen(100)
+local.listen(1)
 
+# Server que hace forwarding de las peticiones a la interfaz local al servidor de la plataforma de agentes
 while True:
     client, addr = local.accept()
+    # socket que se va a conectar a la plataforma
     cp = socket(type = SOCK_STREAM)
+    # recibir el pedido del socket local
     msg = client.recv(1024)
     print(msg)
+    # enviar el request al server
     cp.connect(server)
     cp.send(msg)
+    # Leer la respuesta byte a byte para evitar problemas de bloqueo
     msgcp = cp.recv(1)
     while(msgcp != b''):
+        # enviar la respuesta al socket que originalmente hizo el request a la interfaz local (localhost:8000)
         client.send(msgcp)
         print(msgcp)
+        # continuar leyendo
         msgcp = cp.recv(1)
+    # Cerrar los sockets tanto el remoto como el que responde a la petición local
     cp.close()
     client.close()
 
