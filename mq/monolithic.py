763bbcb877269ae3910b465c6f2b55b6e8db5804
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-11-03 12:17:31 -0500

    Fixed concurrent problem in m.queue.

--- mq/monolithic.py
+++ mq/monolithic.py
@@ -1,4 +1,5 @@
 import socket as sock
+from time import sleep
 from json import loads, dumps
 from threading import Thread, Semaphore
 
@@ -16,42 +17,46 @@ class MessaggeQueue:
         self.s_reader = sock.socket(type=sock.SOCK_DGRAM)
         self.s_reader.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, True)
         self.s_reader.bind(('localhost', self.p_reader)) #FIXME: Cambiar puerto
+        print(f'socket for recieve messages:{self.p_reciever}\nsocket for get messages:{self.p_reader}')
         
-        threads = [
-            Thread(target=self._recieve).run(),
-            Thread(target=self._read).run(),
-        ]
+        Thread(target=self._recieve, daemon=True).start()
+        Thread(target=self._read, daemon=True).start()
+        
         while True:
+            sleep(5)
             pass
-        # for t in threads:
-        #     t.join()
-        
+
     def _recieve(self):
         while True:
+            print('ready for reciever...')
             rawmsg, addr = self.s_reciever.recvfrom(2048)
             msg = loads(rawmsg)
+            print('arrive:', msg,'from:', addr)
+            
             if msg['type'] == 'consummer':
                 msg.update({'ip': addr[0], 'port': addr[1]})
 
             self.sem.acquire()
-            print(self.queue)
+            print('\n'.join(str(m) for m in self.queue))
             self.queue.insert(0, msg)
             self.sem.release()
         
 
     def _read(self):
         while True:
+            print('ready for reader...')
             rawmsg, addr = self.s_reader.recvfrom(128)
             msg = loads(rawmsg)
             if msg != 'get':
-                print(f'Wierd petition from addr {addr}.')
-                return
+                print(f'wierd petition from addr {addr}.')
+                continue
+            print('request recieved from reader:', msg)
 
             self.sem.acquire()
-            msg = self.queue.pop()
+            msg = self.queue.pop() if self.queue else {}
             print(self.queue)
             self.sem.release()
-            self.s_reader.sendto(dumps(msg), addr)
+            self.s_reader.sendto(dumps(msg).encode(), addr)
 
 def main_test():
     mq = MessaggeQueue(8081, 8082)
