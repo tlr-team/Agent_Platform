c77cdf6e066631641210f6402156501828ecd27a
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-11-22 21:05:36 -0500

    [kdmlia] Initial implementation for Id type.
    Type for easy bytes treatment.

--- kademlia/dht.py
+++ kademlia/dht.py
@@ -1,4 +1,4 @@
-ID_LENGTH_BYTES = 128  # FIXME: Especify a Lenght
+ID_LENGTH_BYTES = 20
 
 
 class Dht:
@@ -11,4 +11,28 @@ class Id(int):
     '''
     Helper methods and operators overloads for the XOR logic. 
     '''
+
+    def __init__(self, value):
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__intValue
+
+    @value.setter
+    def set_value(self, value):
+        if (
+            isinstance(value, bytes)
+            and (not value.isdigit() or len(value) != ID_LENGTH_BYTES)
+            or not isinstance(value, int)
+        ):
+            raise Exception(
+                'value {0} is not in the required format.'.format(value)
+            )  # FIXME: Make a custom Exception
+
+        self.__intValue = (
+            value
+            if isinstance(value, int)
+            else abs(int.from_bytes(value, byteorder='little'))
+        )  # TODO: Put the correct byte order.
 
