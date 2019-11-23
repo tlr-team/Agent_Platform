ddc8f64f2fb5ca654af242f429550630a743e39d
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-11-22 22:07:58 -0500

    [kdmlia] Initial implementations for KBucket & BucketList

--- kademlia/bucketlist.py
+++ kademlia/bucketlist.py
@@ -1,5 +1,26 @@
+from .kbucket import KBucket
+from .dht import Id
+
+
 class BucketList:
     '''
     Manages the contacts (peers) in each bucket and the algorithm 
     for adding contacts (peers) to a particular bucket.
     '''
+
+    def __init__(self, _id: Id):
+        self.__our_id = _id
+        self.__buckets = []
+        # first bucket has max range
+        self.__buckets.append(KBucket())
+
+    @property
+    def buckets(self):
+        return self.__buckets
+
+    @property
+    def id(self):
+        return self.__our_id
+
+    def add_contact(self, contact):
+        raise NotImplementedError()
