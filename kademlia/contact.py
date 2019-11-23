3b6f5613576047d9cdd247f8c2944aecb2d90fc1
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-11-22 21:34:40 -0500

    [kdmlia] Initial implementations for Router & Contact

--- kademlia/contact.py
+++ kademlia/contact.py
@@ -1,5 +1,17 @@
+from datetime import datetime
+
+
 class Contact:
     '''
     Mantains the protocol that the contact (peers) uses, 
     wich is used for determinig whether a peer should be tested for eviction.
     '''
+
+    def __init__(self, protocol, contact_id):
+        self.last_seen: datetime = None
+        self.protocol = protocol
+        self.contact_id = contact_id
+        self.touch()
+
+    def touch(self):
+        self.last_seen = datetime.now()
--- kademlia/router.py
+++ kademlia/router.py
@@ -2,3 +2,6 @@ class Node:
     '''
     Manages peer (node) lookups for acquiring nearest peers and finding key-value pairs.
     '''
+
+    def __init__(self, node=None):
+        self.__Node = node
