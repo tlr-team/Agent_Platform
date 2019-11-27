460acb96028504423b53bf8d30eb337fbe8428e4
Roberto Marti Cedeño <rmarticedeno@gmail.com>
2019-11-27 09:50:28 -0500

    [RT] Fixed minor testing issues

--- rt/router.py
+++ rt/router.py
@@ -3,12 +3,15 @@ from time import sleep
 from json import loads, dumps
 from threading import Thread, Semaphore
 from queue import Queue
-from utils.network import Decode_Response,Encode_Request,Send_Broadcast_Message,Sock_Reader,Tcp_Message,Udp_Message
+from network import Decode_Response,Encode_Request,Send_Broadcast_Message,Sock_Reader,Tcp_Message,Udp_Message
 from io import BytesIO
 
 # Funcionamiento del Router:
 # Hilo1 busca un listado de mq (similar al cliente) y pide un request y lo encola en una lista si esta esta vacia (ojo, semaforo)
 # Hilo2 desencola el request si existe , lo procesa y se conecta finalmente con el cliente con el resultado final
+
+def Get_UdpMessage(socket):
+    return  socket.recvfrom(1024)[0]
 
 # Clase base para el router
 class Router:
@@ -17,20 +20,25 @@ class Router:
         #mutex 
         self.channel = []
         self.mutex = Semaphore()
-        self.Broadcast_Address = ""
-        self.Broadcast_Port = 8900
-        self.am_ip = ""
-        self.sm_ip = ""
-        self.bd_port = 10003
-        Thread(target=self._recieve,deamon=True).start()
-        Thread(target=self._resolve,deamon=True).start()
+        self.Broadcast_Address = "10.10.10.255"
+        self.Broadcast_Port = 10002
+        self.am_ip = "127.0.0.1"
+        self.sm_ip = "10.10.10.5"
+        self.bd_port = 9342
+
+    def serve(self):
+        Thread(target=self._recieve,daemon=True,name="recieve").start()
+        Thread(target=self._resolve,daemon=True,name="resolve").start()
+        while(True):
+            sleep(5)
+        
 
     # Hilo que se va a conectar al mq para recbir un mensaje
     def _recieve(self):
         while(True):
-            result = Send_Broadcast_Message(Encode_Request({"get":"work"}),self.Broadcast_Address,self.Broadcast_Port,)
+            result = Send_Broadcast_Message('get',self.Broadcast_Address,self.Broadcast_Port,Get_UdpMessage)
 
-            if result != None and not len(self.channel):
+            if result and not len(self.channel):
                 self.mutex.acquire()
                 self.channel.append(Decode_Response(result))
                 self.mutex.release()
@@ -42,11 +50,12 @@ class Router:
     def _resolve(self):
         ''' Hilo que va a procesar el pedido '''
         while(True):
-            self.mutex.acquire
+            self.mutex.acquire()
             if not len(self.channel):
                 self.mutex.release() 
                 sleep(5)
             else:
+                print('channel',self.channel)
                 req = self.channel.pop()
                 self.mutex.release() 
                 # Pedido desde un cliente
@@ -54,14 +63,15 @@ class Router:
                     ip = req["ip"]
                     port = req["port"]
                     info = req["get"]
-                    msg = {"get",info}
+                    msg = {"get":info}
                     response = None
                     if info == "full_list":
-                        response = Tcp_Message(msg,am_ip,self.bd_port)
+                        response = Tcp_Message(msg,self.am_ip,self.bd_port)
                     else:
-                        response = Tcp_Message(msg,sm_ip,self.bd_port)
+                        response = Tcp_Message(msg,self.sm_ip,self.bd_port)
                     #Enviar la respuesta
                     Udp_Message(response,ip,port)
+                    print(response)
                     
                 # Pedido desde un productor
                 else:
@@ -70,3 +80,5 @@ class Router:
                     Tcp_Message(req,self.am_ip,self.bd_port)
                     Tcp_Message(req,self.sm_ip,self.bd_port)
             
+
+Router().serve()
