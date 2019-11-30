7ce8962aba081b29cea74d565c310add59fd1e00
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-11-30 13:43:34 -0500

    [node] Minor bugs

--- kademlia/node.py
+++ kademlia/node.py
@@ -34,19 +34,20 @@ class Node:
     def _serve(self):
         with socket(type=SOCK_DGRAM) as serve_socket:
             serve_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
-            serve_socket.bind("",self.service_port)
-            service_port.listen(10)
-            while(True):
-                msg, addr = local.recvfrom(1024)
-                Thread(target = self._attend, args=(msg,addr), daemon = True).start()
+            serve_socket.bind("", self.service_port)
+            serve_socket.listen(10)
+            while True:
+                msg, addr = serve_socket.recvfrom(1024)
+                Thread(target=self._attend, args=(msg, addr), daemon=True).start()
 
     def _attend(self, msg, addr):
         message = Decode_Response(msg)
 
         if 'ping' in message:
-            #Thread(target = self.ping, args=(msg), daemon = True).start()
+            pass
+            # Thread(target = self.ping, args=(msg), daemon = True).start()
         elif 'pong' in message:
-            Thread(target = self.ping, args=(msg,addr[0],addr[1]), daemon = True).start()
+            Thread(target=self.ping, args=(msg, addr[0], addr[1]), daemon=True).start()
         elif 'store' in message:
             pass
         elif 'find_node' in message:
@@ -66,14 +67,15 @@ class Node:
     def bucket_list(self):
         return self.__bucket_list
 
-	def ping(self, ip, port):
-        Upd_Message({"ping":{}},ip,port)
+    def ping(self, ip, port):
+        Udp_Message({"ping": {}}, ip, port)
 
     def pong(self, ip, port):
-        Upd_Message({"pong":{}},ip,port)
+        Udp_Message({"pong": {}}, ip, port)
 
     def store(self, key: Id, val: str):
-        Upd_Message({"store":{"id":Id,"value":val}},ip,port)
+        pass
+        # Udp_Message({"store": {"id": Id, "value": val}}, ip, port)
 
     def find_node(self, sender: Contact, key: Id):
         raise NotImplementedError()
@@ -85,21 +87,26 @@ class Node:
 
     def handle_ping(self, msg):
         with self.lock:
-            sender_contact = Contact.from_dict(msg['sender'])
+            sender_contact = Contact.from_dict(
+                {**msg['sender'], 'our_id': self.contact.id}
+            )
             self.bucket_list.add_contact(sender_contact)
             Udp_Message({'pong': {}}, *sender_contact.addr)
         # return ourContact
 
     def handle_store(self, msg):  # sender: Contact, key: Id, val: str):
         with self.lock:
-            sender_contact = Contact.from_dict(msg['sender'])
+            sender_contact = Contact.from_dict(
+                {**msg['sender'], 'our_id': self.contact.id}
+            )
             self.bucket_list.add_contact(sender_contact)
             Udp_Message({'store_ret': {}}, *sender_contact.addr)
 
-
     def handle_find_node(self, msg):  # sender: Contact, key: Id):
         with self.lock:
-            sender_contact = Contact.from_dict(msg['sender'])
+            sender_contact = Contact.from_dict(
+                {**msg['sender'], 'our_id': self.contact.id}
+            )
             self.bucket_list.add_contact(sender_contact)
             # TODO
         # raise NotImplementedError()
@@ -107,7 +114,9 @@ class Node:
 
     def hanlde_find_value(self, msg):  # sender: Contact, key: Id):
         with self.lock:
-            sender_contact = Contact.from_dict(msg['sender'])
+            sender_contact = Contact.from_dict(
+                {**msg['sender'], 'our_id': self.contact.id}
+            )
             self.bucket_list.add_contact(sender_contact)
             # TODO
         # raise NotImplementedError()
