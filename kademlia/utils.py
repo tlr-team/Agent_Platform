57c66f47b45d292a24470a9b35d146e0ffb60c32
Roberto Marti Cedeño <rmarticedeno@gmail.com>
2019-11-30 11:42:44 -0500

    [Kdemlia][utils] Added Sorted_Queur Class

--- kademlia/utils.py
+++ kademlia/utils.py
@@ -8,3 +8,46 @@ def shared_prefix(*args):
             break
         i += 1
     return args[0][:i]
+
+class node:
+    def __init__(self,value,tree_constant):
+        self.id
+        self.value = id ^ tree_constant
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __ne__(self, other):
+        return self.value != other.value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+
+class Sorted_Queue:
+    def __init__(self, k, main_id):
+        self.k = k
+        self.queue = []
+        self.mid = main_id
+    
+    def __add(nde: node):
+        for i in range(0, len(self.queue)):
+            if nde < self.queue:
+                self.queue.insert(i, nde)
+        if(len(self.queue) > k):
+            self.queue.pop()
+
+    def add(id):
+        self.add(node(id,self.main_id))
+
+
+    def getall():
+        return [ i.value for i in self.queue ]
