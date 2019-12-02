2dfe84bf8b0b34891ce45db5f27c124ebc27e142
Roberto Marti Cedeño <rmarticedeno@gmail.com>
2019-12-02 16:56:41 -0500

    [Utils] Leader Election Algorithm added

--- utils/leader_election.py
+++ utils/leader_election.py
@@ -1,15 +1,35 @@
 from socket import socket, SOCK_DGRAM, SOL_SOCKET, SO_REUSEADDR, SOCK_STREAM, SO_BROADCAST
 from time import sleep
 from json import dumps, loads
-from .network import Get_Subnet_Host_Number,Send_Broadcast_Message,Decode_Response,Encode_Request
+from .network import Get_Subnet_Host_Number,Send_Broadcast_Message,Decode_Response,Encode_Request,Get_Broadcast_Ip,Discovering,Get_Subnet_Host_Number
+from threading import Thread
 
 class Leader_Election:
-    # primero llegua y pregunta quien es el leader ,si recibe un "" convoca a elecciones
-    # si el leader que recibe no esta, convoca a elecciones
+    def __init__(self, ip, mask, port):
+        self.brd = Get_Broadcast_Ip(ip,mask)
+        self.discover = Discovering(port,self.brd,,8)
+        self.mask = mask
+        self.ip = ip
+        self.im_leader = False
+        self.iwas_leader = False
+        self.leader = None
+        Thread(target=self._check_leader,daemon = True).start()
 
-    # el lider
-    # cuando es elegido, realiza un broadcast con su id
-    # cuando recibe un pedido de elecciones, este deja de ser lider
+    def _check_leader(self, time = 10):
+        while(True):
+            ips = self.discover.partners.keys()
+            self.leader = ips.sort(key=lambda x : Get_Subnet_Host_Number(x,self.mask))[-1] if len(self.discover.partners) else None
+            if self.leader:
+                if self.ip == self.leader:
+                    self.im_leader = True
+                    self.iwas_leader = True
+                else:
+                    self.im_leader = False
+                    self.iwas_leader = False
+            sleep(time)
 
-    # 
-    pass
+    def Im_Leader(self):
+        return self.im_leader
+
+    def Leader(self):
+        return self.leader
