f8410366f8b2ee96b2df8e62be61b578d4dae269
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-12-04 14:34:49 -0500

    [kdmlia] Ensure contacts.

--- kademlia/contact.py
+++ kademlia/contact.py
@@ -1,5 +1,6 @@
 from time import monotonic
 from hashlib import sha1
+from json import loads, dumps
 
 
 class Contact:
@@ -8,19 +9,33 @@ class Contact:
     wich is used for determinig whether a peer should be tested for eviction.
     '''
 
-    def __init__(self, addr):
+    def __init__(self, ip, port, id):
+        assert isinstance(port, int) and isinstance(id, int) and isinstance(port, str)
         self.last_seen = None
-        # self.protocol = protocol
-        self.addr = addr
-        self.id = int(sha1((':'.join((addr[0], str(addr[1])))).encode()).hexdigest())
+        self.ip, self.port = ip, port
+        self.id = (
+            id if id else int(sha1((':'.join((ip, str(port)))).encode()).hexdigest())
+        )
 
     @property
-    def to_dict(self):
-        return {'addr': self.addr, 'id': self.id}
+    def to_json(self):
+        return dumps({'ip': self.ip, 'port': self.port, 'id': self.id})
 
     @staticmethod
-    def from_dict(_dict,):
-        return Contact(_dict['addr'])
+    def from_json(jsn_s):
+        _dict = loads(jsn_s)
+        return Contact(_dict['ip'], _dict['port'])
+
+    def __str__(self):
+        return f'({self.id},{self.ip},{self.port})'
+
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __ne__(self, other):
+        return self.id != other.id
 
     def touch(self):
         self.last_seen = monotonic()
