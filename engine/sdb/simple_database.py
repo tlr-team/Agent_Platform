96d23735f4b513e5e3a16d11c9fa77aec12da09f
Roberto Marti Cedeño <rmarticedeno@gmail.com>
2019-12-04 14:09:57 -0500

    [SDB] Simple Database Class and script added

---
+++ engine/sdb/simple_database.py
@@ -0,0 +1,48 @@
+from ..utils.network import Tcp_Message
+from threading import Thread, Lock
+from random import randint
+
+class SimpleDataBase:
+    def __init__(self, ip , port):
+        '''
+        Clase que mantiene un funcionamiento Basico de una Bd (Thread Safe)
+        '''
+        self.dbs = {}
+        self.lock = Lock()
+
+    def _insert(self, tag, agent):
+        '''
+        Inserta un nuevo agente y si existe, acutaliza su tiempo de vida
+        '''
+        with self.lock:
+            if not tag in self.dbs:
+                self.dbs[tag] = [(agent,6)]
+            else:
+                if not agent in self.dbs[tag]:
+                    self.dbs[tag].append(agent,6)
+                else:
+                    self.dbs[tag][1] = 6
+
+    def _get(self,tag):
+        '''
+        Devuelve una lista con a lo sumo 3 agentes que tienen ese servicio
+        '''
+        with self.lock:
+            n_data = len(self.dbs.keys()) 
+            response = []
+            if n_data:
+                if n_data <= 3:
+                    for i in range(0,n_data):
+                        response.append(self.dbs[i][0])
+                else:
+                    a = randint(0,n_data-1)
+                    b = randint(0,n_data-1)
+                    while(b == a):
+                        b = randint(0,n_data-1)
+                    c = randint(0,n_data-1)
+                    while(c == a or c == b):
+                        c = randint(0,n_data-1)
+                    choice = [a,b,c]
+                    for i in choice:
+                        response.append(self.dbs[tag][i][0])
+
