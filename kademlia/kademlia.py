50728f0c7a50c1cfbf6cd783e83b9663008c35db
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-12-05 13:37:55 -0500

    [kdmlia] Log fix in exposed_ping,exposed_init

--- kademlia/kademlia.py
+++ kademlia/kademlia.py
@@ -20,15 +20,18 @@ DefaultBSize = 160
 
 
 class KademliaProtocol(Service):
-    def __init__(self, storage: StorageManager, b=default_BSize, k=default_KSize):
+    def __init__(self, storage: StorageManager, b=DefaultBSize, k=DefaultKSize):
         super(KademliaProtocol, self).__init__()
         self.k, self.b = k, b
         self.storage_manager = storage  # TODO: use it
         self.lock = Lock()
         self.service_port = 9000
         self.db = {}
+        self.db_lock = Lock()
         self.logger = getLogger(name='KademliaProtocol')
         self.initialized = False
+        self.bucket_list: BucketList
+        self.contact: Contact
 
     # region RemoteCall functions
     def exposed_init(self, contact):
@@ -41,7 +44,7 @@ class KademliaProtocol(Service):
         self.logger.debug(f'exposed_init :: Node Initialized (id:{self.contact.id}).')
         self.initialized = True
         self.logger.debug(
-            f'exposed_init :: Initialized(id:{self.contact.id},k:{self.k},b:{self.b}'
+            f'exposed_init :: Node Initialized(id:{self.contact.id},k:{self.k},b:{self.b}'
         )
         return True
 
@@ -50,8 +53,10 @@ class KademliaProtocol(Service):
             self.logger.error(f'exposed_ping :: Node not initialized.')
             return False
         sender = Contact.from_json(sender)
-        self.logger.info(
-            f'exposed_ping :: <Ping> from {sender.addr[0]}:{sender.addr[1]}.'
+        self.logger.info(f'exposed_ping :: <Ping> from {sender}.')
+        self.update_contacts(sender)
+        self.logger.debug(f'exposed_ping :: contact:{sender} stored.')
+        return self.contact.to_json()
         )
         self.add_contacts(sender)
         return True
