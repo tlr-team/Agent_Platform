5ed2bc9880e61558ef109a79d944d7eb7d00fd6a
Roberto Marti Cedeño <rmarticedeno@gmail.com>
2019-12-08 00:31:35 -0500

    [Utils] Double Log Issue Fixed at loggin module

--- engine/utils/logger.py
+++ engine/utils/logger.py
@@ -1,22 +1,24 @@
 import logging
 
-
 def getLogger(name='', level=logging.DEBUG):
     # Gets or creates a logger
     logger = logging.getLogger(name)
 
-    # set log level
-    logger.setLevel(level)
+    if not logger.handlers:
+        # set log level
+        logger.setLevel(level)
 
-    # define file handler and set formatter
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        '[%(asctime)s] - %(levelname)s - %(name)s.%(message)s', datefmt='%H:%M:%S'
-    )
-    handler.setFormatter(formatter)
+        logger.propagate = False
 
-    # add file handler to logger
-    logger.addHandler(handler)
+        # define file handler and set formatter
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '[%(asctime)s] - %(levelname)s - %(name)s.%(message)s', datefmt='%H:%M:%S'
+        )
+        handler.setFormatter(formatter)
+
+        # add file handler to logger
+        logger.addHandler(handler)
     return logger
 
     # Logs
