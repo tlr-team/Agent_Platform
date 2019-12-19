fba619e41c4901d0549cabed351c093b1cd4fcb2
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2019-12-19 16:11:40 -0500

    [modulo] Minor fixes

--- examples/modulo.py
+++ examples/modulo.py
@@ -5,24 +5,25 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from client.agent import AgentService
 
-class modulo(AgentService):
+
+class Modulo(AgentService):
     def __init__(self, ip, port, mask):
-        super(modulo, self).__init__(ip, port, mask)
+        super(Modulo, self).__init__(ip, port, mask)
 
     def exposed_mod(self, a, b):
         '''
-        Finds the remainder after division of one number a by another number b
+            Returns the rest of the division from one number to another number b
         '''
-        if a < 0 or b < 0:
+        if a < 0 or b <= 0:
             return -1
         elif a < b:
             return a
         else:
-            remainder = self.execute('SUBSTRACTOR','substract', a , b)
-            while remainder >= b:
-                remainder = self.execute('SUBSTRACTOR','substract', remainder , b)
+            remainder = a
+            while remainder > b:
+                remainder = self.execute('Substractor', 'subtract', remainder, b)
             return remainder
-            
+
 
                 
 
